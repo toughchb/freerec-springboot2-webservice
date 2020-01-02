0c1d8b83958b3a6f3e4bec81c5c6958ab21a8540
chaehyunbyung <gusqud87@naver.com>
2020-01-02 22:38:50 +0900

    chapter3_2

--- src/main/java/com/jojoldu/book/springboot/domain/posts/Posts.java
+++ src/main/java/com/jojoldu/book/springboot/domain/posts/Posts.java
@@ -15,5 +15,22 @@ import javax.persistence.Id;
 @Entity
 public class Posts
 {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  Long id;
 
+    @Column(length = 500,nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    private String author;
+
+    @Builder
+    public Posts(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
 }
